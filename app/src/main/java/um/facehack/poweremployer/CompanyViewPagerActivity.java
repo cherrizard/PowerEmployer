61967dd73deaffbcb8473a4f92ea6d65bd72ae9a
mischerryous <mischerryous@gmail.com>
2017-11-18 06:55:34 +0800

    Initial commit

---
+++ app/src/main/java/um/facehack/poweremployer/CompanyViewPagerActivity.java
@@ -0,0 +1,13 @@
+package um.facehack.poweremployer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_view_pager);
+    }
+}
---
+++ app/src/main/java/um/facehack/poweremployer/StudentViewPagerActivity.java
@@ -0,0 +1,13 @@
+package um.facehack.poweremployer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_view_pager);
+    }
+}
