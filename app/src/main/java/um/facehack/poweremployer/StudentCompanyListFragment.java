319bc93b818303d0f3ec0fdfad103881b948719f
mischerryous <mischerryous@gmail.com>
2017-11-18 14:30:16 +0800

    available item fragment & company notifications activity card view

--- app/src/main/java/um/facehack/poweremployer/StudentCompanyListFragment.java
+++ app/src/main/java/um/facehack/poweremployer/StudentCompanyListFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AvailableItemFragment.OnFragmentInteractionListener} interface
+ * {@link StudentCompanyListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AvailableItemFragment#newInstance} factory method to
+ * Use the {@link StudentCompanyListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AvailableItemFragment extends Fragment {
+public class StudentCompanyListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class AvailableItemFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public AvailableItemFragment() {
+    public StudentCompanyListFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class AvailableItemFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AvailableItemFragment.
+     * @return A new instance of fragment StudentCompanyListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AvailableItemFragment newInstance(String param1, String param2) {
-        AvailableItemFragment fragment = new AvailableItemFragment();
+    public static StudentCompanyListFragment newInstance(String param1, String param2) {
+        StudentCompanyListFragment fragment = new StudentCompanyListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class AvailableItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_available_item, container, false);
+        return inflater.inflate(R.layout.fragment_student_company_list, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
